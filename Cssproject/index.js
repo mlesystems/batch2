75e160bdecbbacec0e501f008082148da8654863
Ikhlas-saqib <ikhlassaqib1@gmail.com>
2022-01-21 11:06:33 +0530

    Html Project

---
+++ Cssproject/index.js
@@ -0,0 +1,32 @@
+function first1(){
+    document.getElementById("slideimage1").src="images/1.jpg";
+}
+function first2(){
+    document.getElementById("slideimage1").src="images/15.png";
+}
+function second1(){
+    document.getElementById("slideimage2").src="images/2.jpg";
+}
+function second2(){
+    document.getElementById("slideimage2").src="images/16.jpg";
+}
+function third1(){
+    document.getElementById("slideimage3").src="images/17.jpg";
+}
+function third2(){
+    document.getElementById("slideimage3").src="images/19.jpg";
+}
+function fourth1(){
+    document.getElementById("slideimage4").src="images/6.jpg";
+}
+function fourth2(){
+    document.getElementById("slideimage4").src="images/18.jpg";
+}
+setInterval(first1,1000);
+setInterval(first2,2000);
+setInterval(second1,1000);
+setInterval(second2,2000);
+setInterval(third1,1000);
+setInterval(third2,2000);
+setInterval(fourth1,1000);
+setInterval(fourth2,2000);
